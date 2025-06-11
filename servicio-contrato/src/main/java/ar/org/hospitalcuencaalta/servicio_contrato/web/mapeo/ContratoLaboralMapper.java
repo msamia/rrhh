7018b3fc88a59f3933dda023830a0db9fa5fb12f
msamia <msamia@gmail.com>
2025-06-11 19:02:44 -0300

    Clarify empleado mapping

--- servicio-contrato/src/main/java/ar/org/hospitalcuencaalta/servicio_contrato/web/mapeo/ContratoLaboralMapper.java
+++ servicio-contrato/src/main/java/ar/org/hospitalcuencaalta/servicio_contrato/web/mapeo/ContratoLaboralMapper.java
@@ -4,6 +4,7 @@ import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import ar.org.hospitalcuencaalta.servicio_contrato.modelo.ContratoLaboral;
 import ar.org.hospitalcuencaalta.servicio_contrato.web.dto.ContratoLaboralDto;
+import org.mapstruct.Mapping;
 import ar.org.hospitalcuencaalta.servicio_contrato.web.dto.ContratoLaboralDetalleDto;
 
 @Mapper(
@@ -15,8 +16,13 @@ public interface ContratoLaboralMapper {
     // métodos existentes
     ContratoLaboralDto toContratoLaboralDto(ContratoLaboral contrato);
     ContratoLaboral toContratoLaboral(ContratoLaboralDto dto);
+    // "empleado" contiene los datos de EmpleadoRegistry; la entidad ContratoLaboral
+    // únicamente almacena el id, por eso se ignora en esta dirección de mapeo
+    @Mapping(target = "empleado", ignore = true)
     ContratoLaboralDetalleDto toContratoLaboralDetalleDto(ContratoLaboral contrato);
+
     @InheritInverseConfiguration(name = "toContratoLaboralDetalleDto")
+    @Mapping(target = "empleadoId", source = "empleado.id")
     ContratoLaboral toContratoLaboralFromDetalleDto(ContratoLaboralDetalleDto dto);
 
     // ALIAS para tu servicio
