0c46b6b631422ba3d69998541c906ac5e986450b
msamia <msamia@gmail.com>
2025-06-23 06:48:25 -0300

    fix: ignore unmapped relations in mappers

--- servicio-nomina/src/main/java/ar/org/hospitalcuencaalta/servicio_nomina/web/mapeo/LiquidacionMapper.java
+++ servicio-nomina/src/main/java/ar/org/hospitalcuencaalta/servicio_nomina/web/mapeo/LiquidacionMapper.java
@@ -3,7 +3,13 @@ package ar.org.hospitalcuencaalta.servicio_nomina.web.mapeo;
 import ar.org.hospitalcuencaalta.servicio_nomina.modelo.Liquidacion;
 import ar.org.hospitalcuencaalta.servicio_nomina.web.dto.LiquidacionDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel="spring")
-public interface LiquidacionMapper { LiquidacionDto toDto(Liquidacion e); Liquidacion toEntity(LiquidacionDto d); }
+public interface LiquidacionMapper {
+    LiquidacionDto toDto(Liquidacion e);
+
+    @Mapping(target = "empleado", ignore = true)
+    Liquidacion toEntity(LiquidacionDto d);
+}
 
