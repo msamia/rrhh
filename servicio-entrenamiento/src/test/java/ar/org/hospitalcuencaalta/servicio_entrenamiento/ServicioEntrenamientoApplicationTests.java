44e2f9abba649a9c6c86adfcb8cf71141fe1addf
msamia <msamia@gmail.com>
2025-06-29 08:47:16 -0300

    Fix context load test by setting Kafka group id

--- servicio-entrenamiento/src/test/java/ar/org/hospitalcuencaalta/servicio_entrenamiento/ServicioEntrenamientoApplicationTests.java
+++ servicio-entrenamiento/src/test/java/ar/org/hospitalcuencaalta/servicio_entrenamiento/ServicioEntrenamientoApplicationTests.java
@@ -3,7 +3,11 @@ package ar.org.hospitalcuencaalta.servicio_entrenamiento;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "eureka.client.enabled=false")
+@SpringBootTest(properties = {
+        "eureka.client.enabled=false",
+        "spring.kafka.consumer.group-id=test",
+        "spring.kafka.listener.auto-startup=false"
+})
 class ServicioEntrenamientoApplicationTests {
 
     @Test
