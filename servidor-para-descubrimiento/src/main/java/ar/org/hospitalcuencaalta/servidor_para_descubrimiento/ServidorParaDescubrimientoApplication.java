9efcdf750aaf9febb264fec6923105de181c370d
msamia <msamia@gmail.com>
2025-07-03 20:16:43 -0300

    Ejecutar el servidor Eureka como una aplicación independiente por problemas de verisones con Spring Boot

--- servidor-para-descubrimiento/src/main/java/ar/org/hospitalcuencaalta/servidor_para_descubrimiento/ServidorParaDescubrimientoApplication.java
+++ servidor-para-descubrimiento/src/main/java/ar/org/hospitalcuencaalta/servidor_para_descubrimiento/ServidorParaDescubrimientoApplication.java
@@ -1,5 +1,6 @@
 package ar.org.hospitalcuencaalta.servidor_para_descubrimiento;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
