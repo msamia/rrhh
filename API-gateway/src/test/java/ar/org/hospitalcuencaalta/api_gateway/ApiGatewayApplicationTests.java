05dc24018e63b49fd76cfa08ab8475bcf875cb68
msamia <msamia@gmail.com>
2025-07-13 16:04:09 -0300

    Fix Gateway test to use reactive web environment

--- API-gateway/src/test/java/ar/org/hospitalcuencaalta/api_gateway/ApiGatewayApplicationTests.java
+++ API-gateway/src/test/java/ar/org/hospitalcuencaalta/api_gateway/ApiGatewayApplicationTests.java
@@ -3,7 +3,7 @@ package ar.org.hospitalcuencaalta.api_gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApiGatewayApplicationTests {
 
     @Test
